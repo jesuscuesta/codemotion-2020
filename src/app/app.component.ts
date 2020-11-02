0115cde991ec7ef94f018c358b9b918b7376397f
Jesus Cuesta <jesus@jesuscuesta.net>
2020-11-02 16:04:14 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'codemotion-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'codemotion-micro';
+}
